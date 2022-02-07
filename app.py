f9968c0de75faca0a7d4b465530c8eddbbd14c32
Dmitriy Ignatenko <titarx@gmail.com>
2022-02-07 13:05:33 +0400

    Маршруты и функции представления

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<h1>Проверка</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Привет, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
