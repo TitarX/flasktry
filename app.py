cee19d397bfade78b7709da227fe12a71a646ea6
Dmitriy Ignatenko <titarx@gmail.com>
2022-02-09 21:42:27 +0400

    Применение FlaskBootstrap

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -14,6 +16,11 @@ def user(name):
         'name': name
     }
     return render_template('user.html', user=user)
+
+
+@app.route('/bootstrap/')
+def bootstrap_example():
+    return render_template('bootstrap_example.html')
 
 
 if __name__ == '__main__':
