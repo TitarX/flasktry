0d2f82985371d565b9ad04bb287b35a0fb24f54e
Dmitriy Ignatenko <titarx@gmail.com>
2022-02-10 13:57:52 +0400

    Страницы ошибок. Наследование базовым шаблоном базового шаблона Bootstrap. Ссылки. Статические файлы. Локализация дат и времени.

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+from datetime import datetime
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def front():  # put application's code here
-    return render_template('front.html')
+    return render_template('front.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>/')
@@ -21,6 +24,16 @@ def user(name):
 @app.route('/bootstrap/')
 def bootstrap_example():
     return render_template('bootstrap_example.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
 
 
 if __name__ == '__main__':
